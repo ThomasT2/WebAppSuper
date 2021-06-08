4735b20f46dff85033b1e5401dea09bab56b6d15
ThomasT2 <jintont@student.chalmers.se>
2021-06-09 00:19:38 +0200

    Pictures and bug fixes
    Images in the project instead of hyperlinks. Previous annoying bugs are now fixed and lets hope no more arrive!

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -6,22 +6,106 @@ mongoose.connect('mongodb://localhost:27017/reviews');
 var products = [
 
     new Product({
-        imagePath: 'https://i.pinimg.com/originals/04/e8/21/04e8210c2048e31d2049a7876613f54d.jpg',
-        title: 'Avengers',
-        description: 'superheroes',
+        imagePath: '../images/avengers.png',
+        title: 'The Avengers',
+        description: 'The heroes on Earth defend it against an evil Asgardian! It is fun, lots of action and many funny jokes.',
         rating: 9
     }),
     new Product({
-        imagePath: 'https://m.media-amazon.com/images/M/MV5BMjI5NTM5MDA2N15BMl5BanBnXkFtZTcwNjkwMzQxNw@@._V1_.jpg',
+        imagePath: '../images/battleship.png',
         title: 'Battleship',
-        description: 'dumb action',
+        description: 'Earth is attacked by aliens and some tough guys have to defend it. Bad acting from the entire cast and some stupid scenes.',
         rating: 4
     }),
     new Product({
-        imagePath: 'https://m.media-amazon.com/images/M/MV5BODkyNDQzMzUzOF5BMl5BanBnXkFtZTcwODYyMDEyOA@@._V1_.jpg',
+        imagePath: '../images/dredd.png',
         title: 'Dredd',
-        description: 'underrated hard R',
+        description: 'An underrated sci-fi action film with a lot of cool action scenes and some great characters. A must see for fans of the genre.',
         rating: 8
+    }),
+    new Product({
+        imagePath: '../images/baywatch.png',
+        title: 'Baywatch',
+        description: 'Attractive men and women run around the beach but the movie is awful and uninspiring and lazy and just boring.',
+        rating: 3
+    }),
+    new Product({
+        imagePath: '../images/bride.png',
+        title: 'Bridesmaids',
+        description: 'In a group of women one of them is getting married. What happens next is a bonkers comedy that will make you laugh like crazy.',
+        rating: 8
+    }),
+    new Product({
+        imagePath: '../images/aliens.png',
+        title: 'Aliens',
+        description: 'This time it is war! This is the greatest movie ever made and no one can change my mind. One of the best sequels as well.',
+        rating: 10
+    }),
+    new Product({
+        imagePath: '../images/deep.png',
+        title: 'Deep Rising',
+        description: 'A small cult classic from the 90s that proves a lot of imagination and ripping off Alien can be good. A fun action film.',
+        rating: 6
+    }),
+    new Product({
+        imagePath: '../images/speed.png',
+        title: 'Speed',
+        description: 'Get ready for rush hour. The summer heat is coming to LA and the heat in the theater is rising when the action and tension is sky-high in this masterpiece.',
+        rating: 9
+    }),
+    new Product({
+        imagePath: '../images/gods.png',
+        title: 'Gods of Egypt',
+        description: 'Some Gods in the desert fight each other in a bunch of boring and bland scenes. The film is full of CGI and loud noises that it hurts.',
+        rating: 4
+    }),
+    new Product({
+        imagePath: '../images/stardust.png',
+        title: 'Stardust',
+        description: 'Although Lord of the Rings and Harry Potter own the Fantasy genre this film can not be overlooked. Stardust has a unique spark that makes it incredible.',
+        rating: 9
+    }),
+    new Product({
+        imagePath: '../images/planet.png',
+        title: 'Treasure Planet',
+        description: 'What turned out to be a box-office bomb is now a beloved classic that has heart and humor for the entire family. A must see for Disney fans.',
+        rating: 9
+    }),
+    new Product({
+        imagePath: '../images/rush.png',
+        title: 'Rush Hour',
+        description: 'Two odd cops team up and learn to work together in this action comedy that has something for everyone. Prepare some popcorn.',
+        rating: 8
+    }),
+    new Product({
+        imagePath: '../images/parasite.png',
+        title: 'Parasite',
+        description: 'The South Korean movie that dominated the Academy Awards and surprised every movie goers around the globe.',
+        rating: 9
+    }),
+    new Product({
+        imagePath: '../images/jaws.png',
+        title: 'Jaws',
+        description: 'The first blockbuster ever. Steven Spielberg teams up with John Williams to deliver a perfect horror drama that makes you stay out of the water.',
+        rating: 10
+    }),
+    new Product({
+        imagePath: '../images/lone.png',
+        title: 'The Lone Ranger',
+        description: 'A gigantic flop that cost the studio millions of dollar. However, this western does have a certain charm to it and should not be overlooked.',
+        rating: 7
+    }),
+    new Product({
+        imagePath: '../images/clash.png',
+        title: 'Clash of the Titans 2010',
+        description: 'Sam Worthington became a star with the hit movie Avatar but now it seems that his career will go straight down the toilet with this stinker.',
+        rating: 5
+    }),
+    new Product({
+        imagePath: '../images/jakten.png',
+        title: 'Jakten',
+        description: 'This danish movie, also known as The Hunt, is a powerful drama with a strong lead acting performance and many scenes that punch you hard in the gut.',
+        rating: 10
     })
 ];
 
